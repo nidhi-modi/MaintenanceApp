d080f32e6c9277165dcbe3806d607a8431a69a1e
Nidhi Modi <modini@tandg.global>
2022-06-29 12:08:28 +1200

    change bar style to default

--- components/CustomStatusBar.js
+++ components/CustomStatusBar.js
@@ -1,25 +1,26 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, StatusBar } from 'react-native';
-import { SafeAreaView, SafeAreaProvider, useSafeAreaInsets} from 'react-native-safe-area-context';
+import {Text, View, StyleSheet, StatusBar} from 'react-native';
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
+const CustomStatusBar = ({
+  backgroundColor,
+  barStyle = 'default',
+  //add more props StatusBar
+}) => {
+  const insets = useSafeAreaInsets();
 
-const CustomStatusBar = (
-  {
-    backgroundColor,
-    barStyle = "dark-content",
-    //add more props StatusBar
-  }
-) => { 
-   
-   const insets = useSafeAreaInsets();
-
-   return (
-     <View style={{ height: insets.top, backgroundColor }}>
-        <StatusBar
-          animated={true}
-          backgroundColor={backgroundColor}
-          barStyle={barStyle} />
-     </View>
-   );
-}
+  return (
+    <View style={{height: insets.top, backgroundColor}}>
+      <StatusBar
+        animated={true}
+        backgroundColor={backgroundColor}
+        barStyle={barStyle}
+      />
+    </View>
+  );
+};
 export default CustomStatusBar;
